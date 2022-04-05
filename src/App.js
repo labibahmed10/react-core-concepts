23e51d3dedb3e97145e42c5ee40988a7b25188f3
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-05 23:51:35 +0600

    installed firebase first time again working on it

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import About from "./Componenets/About/About";
-import DetailFriend from "./Componenets/DetailOfSingleFriend/DetailFriend";
-import FakePost from "./Componenets/FakePost/FakePost";
-import Friends from "./Componenets/Friends/Friends";
-import Home from "./Componenets/Home/Home";
-import Navbar from "./Componenets/Navbar/Navbar";
-import NotFound from "./Componenets/NotFound/NotFound";
-import SinglePost from "./Componenets/SinglePost/SinglePost";
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-
-        <Route path="/friends" element={<Friends></Friends>}></Route>
-
-        <Route path="/SingleFriend/:username" element={<DetailFriend></DetailFriend>}></Route>
-
-        <Route path="/posts" element={<FakePost></FakePost>}>
-          <Route path=":postID" element={<SinglePost></SinglePost>}></Route>
-        </Route>
-
-        <Route path="/about" element={<About></About>}></Route>
-
-        <Route path="*" element={<NotFound> </NotFound>}></Route>
-      </Routes>
+      <button>Google Sign In</button>
     </div>
   );
 }
--- src/Componenets/Home/Home.js
+++ src/Componenets/Home/Home.js
@@ -10,3 +10,21 @@ const Home = () => {
 };
 
 export default Home;
+
+/*  copy them all to app.js that will work
+  <Navbar></Navbar>
+     <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+
+          <Route path="/friends" element={<Friends></Friends>}></Route>
+
+          <Route path="/SingleFriend/:username" element={<DetailFriend></DetailFriend>}></Route>
+
+          <Route path="/posts" element={<FakePost></FakePost>}>
+            <Route path=":postID" element={<SinglePost></SinglePost>}></Route>
+          </Route>
+
+          <Route path="/about" element={<About></About>}></Route>
+
+          <Route path="*" element={<NotFound> </NotFound>}></Route>
+     </Routes> */
