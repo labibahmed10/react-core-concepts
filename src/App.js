188bbeb9f92f524c74e2e524e73a9e8187b1f90d
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-06 16:28:07 +0600

    installed firebase first time again working on it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import app from "./firebase.init";
+import { useState } from "react";
+
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const [userInfo, setUserInfo] = useState({});
+
+  const signIn = () => {
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        setUserInfo(res.user);
+        console.log(res.user);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const signOuthandle = () => {
+    signOut(auth)
+      .then(() => {
+        setUserInfo({});
+      })
+      .catch((error) => setUserInfo(error));
+  };
   return (
     <div>
-      <button>Google Sign In</button>
+      {userInfo.displayName ? (
+        <button onClick={signOuthandle}>Google Sign Out</button>
+      ) : (
+        <button onClick={signIn}>Google Sign In</button>
+      )}
+
+      <h1>{userInfo.displayName}</h1>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtdxlWIBGv9VP1m18-bOnn65_iRBsOMAA",
+  authDomain: "react-firebase-authentic-8e3d7.firebaseapp.com",
+  projectId: "react-firebase-authentic-8e3d7",
+  storageBucket: "react-firebase-authentic-8e3d7.appspot.com",
+  messagingSenderId: "634485677990",
+  appId: "1:634485677990:web:837a0b4fd926d7a8dcf967",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
